519b91ee70349b6e47770c7d8294980b4ce38e1c
Daniel Rodrigues da Silva <danielrs87@gmail.com>
2024-02-01 21:03:30 -0300

    first commit

---
+++ atividade.py
@@ -0,0 +1,11 @@
+# Usando for loop
+for andar in range(20, 0, -1):
+    if andar != 13:
+        print(andar)
+
+# Usando while loop
+andar = 20
+while andar >= 1:
+    if andar != 13:
+        print(andar)
+    andar -= 1
